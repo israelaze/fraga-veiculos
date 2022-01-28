e371aa1ed20c551f9d5263808b9b184e6de06321
Israel Fraga <israelaze@gmail.com>
2022-01-28 15:32:25 -0300

    Tratamento de excessões

---
+++ src/main/java/br/com/fragaveiculos/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,58 @@
+package br.com.fragaveiculos.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	//Status code 500
+	@ExceptionHandler(ServiceException.class)
+	public ResponseEntity<StandardError> exception(Exception e, HttpServletRequest request){
+		
+		Integer status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+		String error = "ERROR";
+		
+		StandardError standardError = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(standardError);
+	}
+	
+	//Status code 400
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<StandardError> badRequest(BadRequestException e, HttpServletRequest request){
+		
+		Integer status = HttpStatus.BAD_REQUEST.value();
+		String error = "ERROR";
+		
+		StandardError standardError = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+	}
+
+	//Status code 404
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		
+		Integer status = HttpStatus.NOT_FOUND.value();
+		String error = "ERROR";
+		
+		StandardError standardError = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+	
+	//Status code 422
+	@ExceptionHandler(UnprocessableEntityException.class)
+	public ResponseEntity<StandardError> unprocessable(UnprocessableEntityException e, HttpServletRequest request){
+		
+		Integer status = HttpStatus.UNPROCESSABLE_ENTITY.value();
+		String error = "ERROR";
+		
+		StandardError standardError = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standardError);
+	}
+}
