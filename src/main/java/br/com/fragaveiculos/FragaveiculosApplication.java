709fa7bd15f4ae3c8b75555db82fb7ef9ac24db1
Israel Fraga <israelaze@gmail.com>
2022-01-28 13:58:31 -0300

    Projeto inicial criado

---
+++ src/main/java/br/com/fragaveiculos/FragaveiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fragaveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragaveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragaveiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fragaveiculos/FragaveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fragaveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragaveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
