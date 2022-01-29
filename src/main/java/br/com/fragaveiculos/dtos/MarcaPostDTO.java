dfc711d32c3f117e64fe198b8a67d1d0d3f0231d
Israel Fraga <israelaze@gmail.com>
2022-01-29 18:55:40 -0300

    DTOs de Marca

---
+++ src/main/java/br/com/fragaveiculos/dtos/MarcaPostDTO.java
@@ -0,0 +1,12 @@
+package br.com.fragaveiculos.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MarcaPostDTO {
+
+	private String nome;
+	private String nacionalidade;
+}
---
+++ src/main/java/br/com/fragaveiculos/dtos/MarcaPutDTO.java
@@ -0,0 +1,13 @@
+package br.com.fragaveiculos.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MarcaPutDTO {
+
+	private Integer id;
+	private String nome;
+	private String nacionalidade;
+}
