259972b1467a38cb2dcb072cc36d5effda9cda8b
Israel Fraga <israelaze@gmail.com>
2022-02-01 18:46:19 -0300

    Entidade Cliente e seu repositório

---
+++ src/main/java/br/com/fragaveiculos/entities/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.fragaveiculos.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 60, nullable = false)
+	private String nome;
+	
+	@Column(length = 14, nullable = false, unique = true)
+	private String cpf;
+	
+	@Column(length = 50)
+	private String email;
+	
+	@Column(length = 20, nullable = false)
+	private String telefone;
+
+}
---
+++ src/main/java/br/com/fragaveiculos/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package br.com.fragaveiculos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fragaveiculos.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	Cliente findByCpf(String cpf);
+}
